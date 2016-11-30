471f9d1dafc07a6995e555fe523244cccf79f9c7
Shirhen Ince <shirhen23@gmail.com>
2016-12-01 01:50:49 +0200

    show list

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
-import UserList from './userList.js';
 import volunteers from '../data/volunteers.json';
+import meetings from '../data/meetings.json';
+import MeetingModal from './meetingModalPatient.js';
 
 class App extends Component {
+  constructor(props) {
+   super(props);
+   this.renderMeeting = this.renderMeeting.bind(this);
+  }
+  renderMeeting({key,index,isScrolling,isVisible,style}){
+    return <Meeting id={key} style={style} index={index} meeting={meetings.meetings[index]} volunteer={volunteers.volunteers[index]}/>;
+  }
   render() {
     return (
       <div className="App">
@@ -15,7 +23,7 @@ class App extends Component {
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
-        <UserList users={volunteers.volunteers} userHeight={70} listWidth={500} listHeight={500}/>
+        <MeetingModal meeting={meetings.meetings[0]} volunteers={volunteers.volunteers} volunteer={volunteers.volunteers[0]}/>
       </div>
     );
   }
